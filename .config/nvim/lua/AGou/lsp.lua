ee43ae5602a46f2abd9ad1c0d0110481e1b2a621
505384662 <31720616+505384662@users.noreply.github.com>
2022-04-29 00:07:02 +0800

    Update lsp.lua

--- .config/nvim/lua/AGou/lsp.lua
+++ .config/nvim/lua/AGou/lsp.lua
@@ -50,7 +50,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- -------------------------- lsp server ----------------------
-local servers = { 'gopls', 'bashls', 'sqls','lua'}
+local servers = { 'gopls', 'bashls', 'sqls','sumneko_lua'}
 ---------------------------------------------------------------
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
