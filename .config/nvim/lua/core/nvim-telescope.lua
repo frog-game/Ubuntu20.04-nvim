97ab790788bdfababca6accc27f3cf7d95d86a4c
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-05 12:38:20 +0800

    Update nvim-telescope.lua

--- .config/nvim/lua/core/nvim-telescope.lua
+++ .config/nvim/lua/core/nvim-telescope.lua
@@ -1,168 +1,146 @@
-local status_ok, telescope = pcall(require, "telescope")
-if not status_ok then
+local ok, telescope = pcall(require, "telescope")
+if not ok then
+  vim.notify "Could not load telescope"
   return
 end
 
-local actions = require "telescope.actions"
-local trouble = require("trouble.providers.telescope")
--- only support linux
--- telescope.load_extension "media_files"
+local extensions = {
+  "fzf",
+  "hop",
+  "file_browser",
+  "project",
+  "media_files",
+  "notify",
+  "dap",
+}
 
+-- Hot-reloaded function for telescope-hop
+if pcall(require, "plenary") then
+  RELOAD = require("plenary.reload").reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
 
 telescope.setup {
   defaults = {
-
-    prompt_prefix = "> ",
-    selection_caret = " ",
-    path_display = { shorten = 10 },
-    borderchars = {
-    prompt = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    results = { "─", "▐", "─", "│", "╭", "▐", "▐", "╰" },
-    -- results = {' ', '▐', '▄', '▌', '▌', '▐', '▟', '▙' };
-    preview = { " ", "│", " ", "▌", "▌", "╮", "╯", "▌" },
-  },
-    file_ignore_patterns = {
-    "vendor/*",
-    "%.lock",
-    "__pycache__/*",
-    "%.sqlite3",
-    "%.ipynb",
-    "node_modules/*",
-    "%.jpg",
-    "%.jpeg",
-    "%.png",
-    "%.svg",
-    "%.otf",
-    "%.ttf",
-    ".git/",
-    "%.webp",
-    ".dart_tool/",
-    ".github/",
-    ".gradle/",
-    ".idea/",
-    ".settings/",
-    ".vscode/",
-    "__pycache__/",
-    "build/",
-    "env/",
-    "gradle/",
-    "node_modules/",
-    "target/",
-    "%.pdb",
-    "%.dll",
-    "%.class",
-    "%.exe",
-    "%.cache",
-    "%.ico",
-    "%.pdf",
-    "%.dylib",
-    "%.jar",
-    "%.docx",
-    "%.met",
-    "smalljre_*/*",
-    ".vale/",
-    "%.burp",
-    "%.mp4",
-    "%.mkv",
-    "%.rar",
-    "%.zip",
-    "%.7z",
-    "%.tar",
-    "%.bz2",
-    "%.epub",
-    "%.flac",
-    "%.tar.gz",
-  },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    prompt_prefix = " ",
+    selection_caret = "➜ ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+    },
+    file_sorter = require("telescope.sorters").get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    use_less = true,
+    path_display = {},
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
     mappings = {
       i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-c>"] = actions.close,
---
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-
-        ["<CR>"] = actions.select_default,
-        ["<C-s>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-        ["<c-t>"] = trouble.open_with_trouble,
-      },
-
-      n = {
-        ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default,
-        ["<C-s>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
-
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
-
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
-
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
-
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
-
-        ["?"] = actions.which_key,
-        ["<c-t>"] = trouble.open_with_trouble,
+        -- IMPORTANT
+        -- either hot-reloaded or `function(prompt_bufnr) telescope.extensions.hop.hop end`
+        ["<C-h>"] = R("telescope").extensions.hop.hop, -- hop.hop_toggle_selection
+        -- custom hop loop to multi selects and sending selected entries to quickfix list
+        ["<C-space>"] = function(prompt_bufnr)
+          local opts = {
+            callback = require("telescope.actions").toggle_selection,
+            loop_callback = require("telescope.actions").send_selected_to_qflist,
+          }
+          require("telescope").extensions.hop._hop_loop(prompt_bufnr, opts)
+        end,
       },
     },
+
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
   },
-  pickers = {
-      find_files = {
-      mappings = {
-        n = {
-          ["cd"] = function(prompt_bufnr)
-            local selection = require("telescope.actions.state").get_selected_entry()
-            local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-            require("telescope.actions").close(prompt_bufnr)
-            -- Depending on what you want put `cd`, `lcd`, `tcd`
-            vim.cmd(string.format("silent lcd %s", dir))
-          end
-        }
-      }
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
+    hop = {
+      -- the shown `keys` are the defaults, no need to set `keys` if defaults work for you ;)
+      -- keys = {
+      --   "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+      --   "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+      -- },
+      -- Highlight groups to link to signs and lines; the below configuration refers to demo
+      -- sign_hl typically only defines foreground to possibly be combined with line_hl
+      -- sign_hl = { "WarningMsg", "Title" },
+      -- optional, typically a table of two highlight groups that are alternated between
+      -- line_hl = { "CursorLine", "Normal" },
+      -- options specific to `hop_loop`
+      -- true temporarily disables Telescope selection highlighting
+      clear_selection_hl = false,
+      -- highlight hopped to entry with telescope selection highlight
+      -- note: mutually exclusive with `clear_selection_hl`
+      trace_entry = true,
+      -- jump to entry where hoop loop was started from
+      reset_selection = true,
+    },
+    file_browser = {
+      theme = "ivy",
+    },
+    media_files = {
+      -- filetypes whitelist
+      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+      filetypes = { "png", "jpg", "mp4", "webm" },
+      find_cwd = "rg",
     },
   },
-  -- extensions = {
-  --   media_files = {
-  --     -- filetypes whitelist
-  --     -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-  --     filetypes = { "png", "webp", "jpg", "jpeg" },
-  --     find_cmd = "rg", -- find command (defaults to `fd`)
-  --   },
-  -- },
 }
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
 
+for i = 1, #extensions do
+  telescope.load_extension(extensions[i])
+end
+
+vim.keymap.set("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files()<CR>")
+vim.keymap.set("n", "<leader>fg", "<cmd>lua require'telescope.builtin'.live_grep()<CR>")
+vim.keymap.set("n", "<leader>f?", "<cmd>lua require'telescope.builtin'.help_tags()<CR>")
+vim.keymap.set("n", "<leader>fh", "<cmd>lua require'telescope.builtin'.oldfiles()<CR>")
+vim.keymap.set("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.marks()<CR>")
+vim.keymap.set("n", "<space>fe", "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<CR>")
+vim.keymap.set("n", "<leader>fp", "<cmd>lua require'telescope'.extensions.project.project{}<CR>")
+vim.keymap.set("n", "<leader>fr", "<cmd>lua require'telescope'.extensions.frecency.frecency()<CR>")
+vim.keymap.set("n", "<leader>fm", "<cmd>lua require'telescope'.extensions.media_files.media_files()<CR>")
+vim.keymap.set("n", "<leader>fn", "<cmd>lua require'telescope'.extensions.notify.notify()<CR>")
+vim.keymap.set("n", "<leader>fde", "<cmd>lua require'telescope'.extensions.dap.commands()<CR>")
+vim.keymap.set("n", "<leader>fdc", "<cmd>lua require'telescope'.extensions.dap.configurations()<CR>")
+vim.keymap.set("n", "<leader>fdb", "<cmd>lua require'telescope'.extensions.dap.list_breakpoints()<CR>")
+vim.keymap.set("n", "<leader>fdv", "<cmd>lua require'telescope'.extensions.dap.variables()<CR>")
+vim.keymap.set("n", "<leader>fdf", "<cmd>lua require'telescope'.extensions.dap.frames()<CR>")
