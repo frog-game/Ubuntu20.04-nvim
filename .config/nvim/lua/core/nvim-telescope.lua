912e3e2aea835b766292cbc79ed3d4e6996efc89
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-05 16:40:03 +0800

    Update nvim-telescope.lua

--- .config/nvim/lua/core/nvim-telescope.lua
+++ .config/nvim/lua/core/nvim-telescope.lua
@@ -35,22 +35,7 @@ telescope.setup {
       "--column",
       "--smart-case",
     },
-    pickers = {
-    find_files = {
-      mappings = {
-        n = {
-          ["cd"] = function(prompt_bufnr)
-            --local selection = require("telescope.actions.state").get_selected_entry()
-            --local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-            require("telescope.actions").close(prompt_bufnr)
-            -- Depending on what you want put `cd`, `lcd`, `tcd`
-            vim.cmd(string.format("silent lcd %s", "/root/work/skynet-blueprint"))
-          end
-        }
-      }
-    },
-  },
-
+ 
     prompt_prefix = " ",
     selection_caret = "➜ ",
     entry_prefix = "  ",
