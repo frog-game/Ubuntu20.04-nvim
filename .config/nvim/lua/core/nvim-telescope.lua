0c02d7345823b80be3de5aaa7379674c65463108
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-05 12:10:46 +0800

    Update nvim-telescope.lua

--- .config/nvim/lua/core/nvim-telescope.lua
+++ .config/nvim/lua/core/nvim-telescope.lua
@@ -84,13 +84,19 @@ telescope.setup {
     },
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+      find_files = {
+      mappings = {
+        n = {
+          ["cd"] = function(prompt_bufnr)
+            local selection = require("telescope.actions.state").get_selected_entry()
+            local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+            require("telescope.actions").close(prompt_bufnr)
+            -- Depending on what you want put `cd`, `lcd`, `tcd`
+            vim.cmd(string.format("silent lcd %s", dir))
+          end
+        }
+      }
+    },
   },
   -- extensions = {
   --   media_files = {
