48ac4b0305a34ebb46e4532ff803aed0c85090ca
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-10 01:07:23 +0800

    Update init.lua

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -53,18 +53,18 @@ require("persistence").setup {
     options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
 }
 -- --------------------------------------
-local tabnine = require('cmp_tabnine.config')
-tabnine:setup({
-    max_lines = 1000;
-    max_num_results = 20;
-    sort = true;
-    run_on_every_keystroke = true;
-    snippet_placeholder = '..';
-    ignored_file_types = { -- default is not to ignore
+-- local tabnine = require('cmp_tabnine.config') 电脑太垃圾太卡了导致我暂时关闭
+-- tabnine:setup({
+  --   max_lines = 1000;
+ --   max_num_results = 20;
+ --   sort = true;
+   --  run_on_every_keystroke = true;
+  -- snippet_placeholder = '..';
+ --  ignored_file_types = { -- default is not to ignore
         -- uncomment to ignore in lua:
         -- lua = true
-    };
-})
+  --  };
+-- })
 
 -- --------------------------------------
 -- require("lsp-colors").setup({
