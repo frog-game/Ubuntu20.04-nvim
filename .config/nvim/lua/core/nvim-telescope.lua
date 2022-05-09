6a44ce69b4a8ea20a3b674baa8e18beb99c78dc2
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-10 00:32:37 +0800

    Update nvim-telescope.lua

--- .config/nvim/lua/core/nvim-telescope.lua
+++ .config/nvim/lua/core/nvim-telescope.lua
@@ -40,7 +40,7 @@ telescope.setup {
       "--smart-case",
     },
  
-    prompt_prefix = "",
+    prompt_prefix = "  ",
     selection_caret = "➜ ",
     entry_prefix = "  ",
     selection_strategy = "reset",
