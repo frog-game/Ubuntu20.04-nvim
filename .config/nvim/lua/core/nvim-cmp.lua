a8278381bc6ef59b927f89aa8b221b5d23ac463a
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-10 01:09:05 +0800

    Update nvim-cmp.lua

--- .config/nvim/lua/core/nvim-cmp.lua
+++ .config/nvim/lua/core/nvim-cmp.lua
@@ -57,7 +57,7 @@ local source_mapping = {
     -- buffer = "[Buffer]",
     nvim_lsp = "[LSP]",
     nvim_lua = "[Lua]",
-    cmp_tabnine = "[TN]",
+    -- cmp_tabnine = "[TN]",
     path = "[Path]",
     emoji = "[Emoji]",
     look = "[Dict]",
@@ -99,12 +99,12 @@ cmp.setup({
                 before = function(entry, vim_item)
                     vim_item.kind = lspkind.presets.default[vim_item.kind]
                     local menu = source_mapping[entry.source.name]
-                    if entry.source.name == 'cmp_tabnine' then
-                        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-                            menu = entry.completion_item.data.detail .. ' ' .. menu
-                        end
-                        vim_item.kind = ''
-                    end
+                    -- if entry.source.name == 'cmp_tabnine' then
+                        --  if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+                         --     menu = entry.completion_item.data.detail .. ' ' .. menu
+                        --  end
+                        --  vim_item.kind = ''
+                   --   end
                     vim_item.menu = menu
                     return vim_item
                 end
