919f986b407262e78ad544efeb1a987e317ed40f
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-10 13:39:12 +0800

    Update lsp.lua

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -44,15 +44,6 @@ Itkey_on_attach = function(client, bufnr)
    buf_set_keymap("n", "gpi", "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", opts)
    buf_set_keymap("n", "gP",  "<cmd>lua require('goto-preview').close_all_win()<CR>", opts)
    buf_set_keymap("n", "gF", "<cmd>lua require('goto-preview').goto_preview_references()<CR>", opts)
-
-
-  -- 代码保存自动格式化formatting
-  if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_command [[augroup Format]]
-   vim.api.nvim_command [[autocmd! * <buffer>]]
-   vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-   vim.api.nvim_command [[augroup END]]
- end
 end
 
 -- Add additional capabilities supported by nvim-cmp
