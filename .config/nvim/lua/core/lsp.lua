88a9073a52a79a8378463aedac313018cf5401c7
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-10 13:25:13 +0800

    Update lsp.lua

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -47,13 +47,13 @@ Itkey_on_attach = function(client, bufnr)
 
 
   -- 代码保存自动格式化formatting
-  if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
-end
+  --if client.resolved_capabilities.document_formatting then
+    --vim.api.nvim_command [[augroup Format]]
+   -- vim.api.nvim_command [[autocmd! * <buffer>]]
+   -- vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+   -- vim.api.nvim_command [[augroup END]]
+ -- end
+--end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
