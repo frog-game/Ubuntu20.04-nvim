fe0d207b8bebcfa864af970689e4508af9b22d01
505384662 <31720616+505384662@users.noreply.github.com>
2022-05-23 23:49:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -100,14 +100,15 @@ fc-list #查看已经安装的
 sudo apt-get -y install ripgrep
 
 #安装zsh和oh-my-zsh
+sudo rm -rf ~/.oh-my-zsh
 sudo apt -y install zsh
 sudo wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
 sudo chsh -s `which zsh`
-git restore .zshrc
-source .zshrc
+git restore ~/.zshrc
+source ~/.zshrc
 
 #go安装
 sudo wget https://dl.google.com/go/go1.17.6.linux-amd64.tar.gz
@@ -122,4 +123,4 @@ sudo apt-get update
 sudo apt-get -y install lazygit
 
 #删除冗余文件
-sudo rm -rf wget-log* .wget-hsts
+sudo rm -rf wget-log* .wget-hsts README.md
