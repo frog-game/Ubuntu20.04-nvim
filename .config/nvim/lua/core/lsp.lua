5bd38e2291fc37069993737009c1f899b848f8ec
505384662 <31720616+505384662@users.noreply.github.com>
2022-06-10 15:10:16 +0800

    Update lsp.lua

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -49,7 +49,7 @@ Itkey_on_attach = function(client, bufnr)
   if client.resolved_capabilities.document_formatting then
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> EslintFixAll]]
+    --vim.api.nvim_command [[autocmd BufWritePre <buffer> EslintFixAll]]
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
     vim.api.nvim_command [[augroup END]]
   end
