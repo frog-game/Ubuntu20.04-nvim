9ba97603c8fceb70a30ad8f33ca6008aa67bcc8d
505384662 <31720616+505384662@users.noreply.github.com>
2022-06-24 17:28:13 +0800

    Update nvim-dap-ui.lua

--- .config/nvim/lua/core/nvim-dap-ui.lua
+++ .config/nvim/lua/core/nvim-dap-ui.lua
@@ -1,42 +1,39 @@
-require("dapui").setup({
+local status_ok, dapui = pcall(require, 'dapui')
+if not status_ok then
+  vim.notify("dapui not found")
+  return
+end
+
+dapui.setup ({
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
     -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
+    expand = { "o", "<2-LeftMouse>", "<CR>" },
+    open = "O",
     remove = "d",
     edit = "e",
     repl = "r",
     toggle = "t",
   },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = vim.fn.has("nvim-0.7"),
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position.
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
+  sidebar = {
+    -- You can change the order of elements in the sidebar
+    elements = {
+      -- Provide as ID strings or tables with "id" and "size" keys
+      {
+        id = "scopes",
+        size = 0.35, -- Can be float or integer > 1
       },
-      size = 40,
-      position = "left",
+      { id = "stacks", size = 0.35 },
+      { id = "watches", size = 0.15 },
+      { id = "breakpoints", size = 0.15 },
     },
-    {
-      elements = {
-        "repl",
-        "console",
-      },
-      size = 10,
-      position = "bottom",
-    },
+    size = 40,
+    position = "left", -- Can be "left", "right", "top", "bottom"
+  },
+  tray = {
+    elements = { "repl" },
+    size = 5,
+    position = "bottom", -- Can be "left", "right", "top", "bottom"
   },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
@@ -47,7 +44,4 @@ require("dapui").setup({
     },
   },
   windows = { indent = 1 },
-  render = {
-    max_type_length = nil, -- Can be integer or nil.
-  }
 })
