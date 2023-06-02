8758dcc6e81072034a235ed1ca92fe7f8e9812c6
frog <31720616+frog-game@users.noreply.github.com>
2023-06-03 00:27:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -183,7 +183,6 @@ yes | sh -c "$(wget -nc -qO- https://gitee.com/oldsyang/ohmyzsh/raw/master/tools
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting 
-zsh
 git restore ~/.zshrc 
 source ~/.zshrc
 chsh -s $(which zsh)  
