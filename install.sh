0e4834056df4e476bc73296dc1de0991842c82cd
frog <31720616+frog-game@users.noreply.github.com>
2023-06-05 11:53:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -180,6 +180,7 @@ sudo rm -rf ~/.oh-my-zsh
 sudo rm -rf ~/.zshrc.*
 sudo apt -y install zsh
 yes | sh -c "$(wget -nc -qO- https://gitee.com/oldsyang/ohmyzsh/raw/master/tools/install.sh)"
+zsh
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting 
