eb15177f1dae61f529aef49eaee64f2598aa6642
frog <31720616+frog-game@users.noreply.github.com>
2024-02-19 13:11:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,7 +53,6 @@ yes | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
-sudo rm -rf ~/.zshrc && wget -cO .zshrc https://raw.githubusercontent.com/frog-game/Ubuntu20.04-nvim/master/zshrc
 zsh
 source ~/.zshrc
 chsh -s $(which zsh)
