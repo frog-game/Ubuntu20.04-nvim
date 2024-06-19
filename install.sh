9bdfcd148599406e1964aa44a3a374302da152d3
frog <31720616+frog-game@users.noreply.github.com>
2024-06-19 15:14:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,9 +39,8 @@ pip install pygments
 
 #安装zsh和oh-my-zsh
 sudo rm -rf ~/.oh-my-zsh
-
 sudo apt -y install zsh
-yes | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+yes | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
